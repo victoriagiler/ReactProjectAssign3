cc93dbfdb03019b099819485427e9b477cd27eb6
wh7580 <wh7580@gmail.com>
2022-11-29 22:28:50 -0800

    Created home component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Home from './Components/Home';
 import Test from './Components/Test';
 
 
@@ -6,6 +7,7 @@ import Test from './Components/Test';
 function App() {
   return (
     <>
+    <Home />
     <h2><Test/></h2>
     </>
   )
