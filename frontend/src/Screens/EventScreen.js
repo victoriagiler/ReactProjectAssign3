9d1dfddab6c8ca1df469d885fa742df20e846bd3
Victoria <104544006+victoriagiler@users.noreply.github.com>
2022-12-01 20:18:01 -0500

    commented out eventscreen

--- frontend/src/Screens/EventScreen.js
+++ frontend/src/Screens/EventScreen.js
@@ -1,57 +1,57 @@
-import React from "react";
-import {Row, Col, Button, Image, ListGroup, Card} from "react-bootstrap";
-import events from "../eventsData";
+// import React from "react";
+// import {Row, Col, Button, Image, ListGroup, Card} from "react-bootstrap";
+// import events from "../eventsData";
 
 
 
 
 
-const EventScreen = () => {
+// const EventScreen = () => {
 
-  const {id} = useParams();
-  const event = 'event data here'
+//   const {id} = useParams();
+//   const event = 'event data here'
 
-  return(
-    <>
-    <a href="#" className="btn btn-light my-3">Go Back</a>
-    <Row>
-    <Image src={event.image}/>
-    </Row>
+//   return(
+//     <>
+//     <a href="#" className="btn btn-light my-3">Go Back</a>
+//     <Row>
+//     <Image src={event.image}/>
+//     </Row>
     
-    <Row>
-      <Card>
-        <Col>
-          <ListGroup>
-          <ListGroup.Item>{event.time}</ListGroup.Item>
-          <ListGroup.Item>{event.date}</ListGroup.Item>
-          <ListGroup.Item>{event.description}</ListGroup.Item>
+//     <Row>
+//       <Card>
+//         <Col>
+//           <ListGroup>
+//           <ListGroup.Item>{event.time}</ListGroup.Item>
+//           <ListGroup.Item>{event.date}</ListGroup.Item>
+//           <ListGroup.Item>{event.description}</ListGroup.Item>
     
-          </ListGroup> 
-        </Col>
-        </Card> 
-        <Col>
-        <Card>
-          <Row>
-            <Col>
-            <strong>${event.price}</strong>
-            </Col>
-          </Row>
-          <Row>
-            <Button 
-            className="btn-block" 
-            type="button"
-            disabled={event.countInStock === 0}
-                >
-                 Get Tickets
-                </Button>
-          </Row>
-        </Card>
-        </Col>
-    </Row>
+//           </ListGroup> 
+//         </Col>
+//         </Card> 
+//         <Col>
+//         <Card>
+//           <Row>
+//             <Col>
+//             <strong>${event.price}</strong>
+//             </Col>
+//           </Row>
+//           <Row>
+//             <Button 
+//             className="btn-block" 
+//             type="button"
+//             disabled={event.countInStock === 0}
+//                 >
+//                  Get Tickets
+//                 </Button>
+//           </Row>
+//         </Card>
+//         </Col>
+//     </Row>
 
-    </>
-  )
+//     </>
+//   )
 
-}
+// }
 
-export default EventScreen
+// export default EventScreen
