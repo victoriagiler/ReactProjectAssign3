1c4320d756f6f9ae060a0b68a19a66424bcb46d5
wh7580 <wh7580@gmail.com>
2022-12-07 21:48:34 -0800

    added img in HomeScreen and scaling in index.csss

--- frontend/src/Screens/HomeScreen.js
+++ frontend/src/Screens/HomeScreen.js
@@ -6,10 +6,14 @@ import events from '../eventsData'
 const Home = () => {
   return (
     <>
+    
+    <img src="https://images.pexels.com/photos/1782146/pexels-photo-1782146.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="Italian Trulli" ></img>
+
     <h1>Latest Events</h1>
+
     <Row>
       {events.map((event)=> (
-        <Col sm={12} md={6} lg={4} xl={3}>
+        <Col sm={12} md={6} lg={4} xl={4}>
         <Event event={event} />
         </Col>
       ))}
