b75dc67fe5eb8e4a58bf9cd7779e0780a5f003c6
Victoria <104544006+victoriagiler@users.noreply.github.com>
2022-12-08 16:25:31 -0500

    fixed layout of EventScreen

--- frontend/src/Screens/EventScreen.js
+++ frontend/src/Screens/EventScreen.js
@@ -1,57 +1,58 @@
-// import React from "react";
-// import {Row, Col, Button, Image, ListGroup, Card} from "react-bootstrap";
-// import events from "../eventsData";
+import React from "react";
+import {Row, Col, Button, Image, ListGroup, Card} from "react-bootstrap";
+import EventScreenStyle from "./EventScreen.css"
+import events from "../eventsData";
 
-
-
-
-
-// const EventScreen = () => {
+const EventScreen = () => {
 
 //   const {id} = useParams();
-//   const event = 'event data here'
+  const event = events
 
-//   return(
-//     <>
-//     <a href="#" className="btn btn-light my-3">Go Back</a>
-//     <Row>
-//     <Image src={event.image}/>
-//     </Row>
+return(
+  <>
+  <a href="#" className="btn btn-light my-3">Go Back</a>
+  <Row>
+  <Image src={event.image}/>
+  </Row>
+  
+  <Row>
     
-//     <Row>
-//       <Card>
-//         <Col>
-//           <ListGroup>
-//           <ListGroup.Item>{event.time}</ListGroup.Item>
-//           <ListGroup.Item>{event.date}</ListGroup.Item>
-//           <ListGroup.Item>{event.description}</ListGroup.Item>
+      <Col md={7}>
+        <ListGroup className="descripCard">
+        <ListGroup.Item>{event.time}</ListGroup.Item>
+        <ListGroup.Item>{event.date}</ListGroup.Item>
+        <ListGroup.Item>{event.description}</ListGroup.Item>
+  
+        </ListGroup> 
+      </Col>
     
-//           </ListGroup> 
-//         </Col>
-//         </Card> 
-//         <Col>
-//         <Card>
-//           <Row>
-//             <Col>
-//             <strong>${event.price}</strong>
-//             </Col>
-//           </Row>
-//           <Row>
-//             <Button 
-//             className="btn-block" 
-//             type="button"
-//             disabled={event.countInStock === 0}
-//                 >
-//                  Get Tickets
-//                 </Button>
-//           </Row>
-//         </Card>
-//         </Col>
-//     </Row>
+      <Col md={5}>
 
-//     </>
-//   )
+      <Card className="cardPrice">
+        <ListGroup.Item className="price"><strong>${event.price}</strong></ListGroup.Item>
+        <ListGroup.Item>
+          <Row>
+          <Button className="btn btn-block" type="button">
+            Get Tickets 
+          </Button>
+          </Row>
+        </ListGroup.Item>
 
-// }
+      </Card>
+      </Col>
+          
+  
+    
+    
+  </Row>
+  
+ 
 
-// export default EventScreen
+  </>
+)
+}
+
+
+export default EventScreen
+
+
