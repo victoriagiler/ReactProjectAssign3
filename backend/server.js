1488a9a58634f60af6bb322995c0f7591b0504c6
wh7580 <wh7580@gmail.com>
2022-12-12 21:50:13 -0800

    backend folder, data FE->BE, server.js, libraries

---
+++ backend/server.js
@@ -0,0 +1,7 @@
+const express  = require('express')
+const eventsData = require('./data/eventsData')
+
+const app = express()
+//two api to be added here
+
+app.listen(5000, console.log('Server is running on port 5000'))
--- frontend/src/Screens/EventScreen.js
+++ frontend/src/Screens/EventScreen.js
@@ -1,7 +1,7 @@
 import React from "react";
 import {Row, Col, Button, Image, ListGroup, Card} from "react-bootstrap";
 import EventScreenStyle from "./EventScreen.css"
-import events from "../eventsData";
+import axios from 'axios'
 import { useParams,Link } from "react-router-dom";
 
 const EventScreen = () => {
