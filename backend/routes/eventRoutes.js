573756c1439a76bf703793ddc3fa82b429724863
Victoria <104544006+victoriagiler@users.noreply.github.com>
2023-01-11 19:39:11 -0500

    productRoutes

---
+++ backend/routes/eventRoutes.js
@@ -0,0 +1,34 @@
+import express from "express";
+import asyncHandler from "express-async-handler"
+import Event from '../models/eventModel.js'
+
+const router= express.Router()
+
+// @desc fetch all events
+// @route GET/api/events/
+// @access public
+
+router.get('/', asyncHandler(async (req, res) =>{
+  const events = await Event.find({})
+  res.json(events)
+
+})
+
+)
+
+// @desc fetch a single event
+// @route GET/api/event/:id
+// @access public
+
+router.get(`/:id`, asyncHandler(async (req,res) => {
+  const event = await Event.findById(req.params.id)
+  if(event){
+    res.json(event)
+  }else {
+    res.status(404).json({message: 'Event not found'})
+  }
+}))
+
+
+
+export default router
--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,15 @@
-import events from './data/eventsData.js'
 import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
+import eventRoutes from './routes/eventRoutes.js'
 
 const app = express()
 dotenv.config()
 connectDB()
 
-app.get('/api/events', (req, res) =>{
-  res.json(events)
-})
+app.use('/api/events', eventRoutes)
 
-app.get('/api/event/:id', (req, res)=>{
-  const event = events.find((e) => e._id === req.params.id)
-  res.json(event)
-})
+
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, console.log(`Server is running on port ${PORT}`))
