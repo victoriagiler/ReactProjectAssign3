a7ae4f0c8d55ba8615e84e89b10affdee4b435b3
wh7580 <wh7580@gmail.com>
2023-01-18 05:51:43 -0800

    errorMiddleware

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 import eventRoutes from './routes/eventRoutes.js'
+import errorHandler from './middleware/errormiddleware.js'
 
 const app = express()
 dotenv.config()
@@ -9,7 +10,7 @@ connectDB()
 
 app.use('/api/events', eventRoutes)
 
-
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, console.log(`Server is running on port ${PORT}`))
