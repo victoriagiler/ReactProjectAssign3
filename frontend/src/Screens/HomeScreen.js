fc81f3d01459f65a492b293a1c3e1b6f1638a801
Katie Park <katie.sang.park@gmail.com>
2023-01-29 09:23:32 -0800

    Loading and message component + import + add HTML

--- frontend/src/Screens/HomeScreen.js
+++ frontend/src/Screens/HomeScreen.js
@@ -3,6 +3,8 @@ import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col } from 'react-bootstrap'
 import Event from '../Components/Event'
 import { listEvents } from '../actions/eventActions'
+import Loader from '../Components/Loader'
+import Message from '../Components/Message'
 
 
 const Home = () => {
@@ -18,18 +20,22 @@ const Home = () => {
   
   return (
     <>
-    
     <img src="https://images.pexels.com/photos/1782146/pexels-photo-1782146.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="Italian Trulli" ></img>
-
     <h1>Latest Events</h1>
-
-    <Row>
-      {events.map((event)=> (
-        <Col sm={12} md={6} lg={4} xl={4}>
-        <Event event={event} />
-        </Col>
-      ))}
-    </Row>
+    {loading ? (
+      <Loader />
+    ) : error ? (
+      <Message variant='danger'>{error}</Message>
+    ) : (
+      <Row>
+        {events.map((event)=> (
+          <Col sm={12} md={6} lg={4} xl={4}>
+          <Event event={event} />
+          </Col>
+        ))}
+      </Row>
+    )
+    }
     </>
     
   )
