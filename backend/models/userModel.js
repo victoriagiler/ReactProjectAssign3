94e4677fc72b1933a487a56be211b87b1c5eeda5
Victoria <104544006+victoriagiler@users.noreply.github.com>
2023-01-31 00:40:07 -0500

    userController and addition to userModel

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose'
+import bcrypt from 'bcryptjs'
 
 const userSchema = mongoose.Schema({
   name:{
@@ -23,6 +24,10 @@ const userSchema = mongoose.Schema({
 { 
   timestamp: true
 })
+
+userSchema.methods.matchPassword = async function(enteredPassword){
+  return await bcrypt.compare(enteredPassword, this.password)
+}
 
 const User = mongoose.model('users', userSchema)
 export default User
