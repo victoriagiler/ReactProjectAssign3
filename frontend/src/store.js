b11df0882b99aa82b6bc486d39bd6cd2fc48768f
Victoria <104544006+victoriagiler@users.noreply.github.com>
2023-02-03 18:50:04 -0500

    User Constants, Actions, Reducers

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,20 +1,28 @@
 import {configureStore, combineReducers} from '@reduxjs/toolkit'
 import { cartReducer } from './reducers/cartReducers'
 import { eventListReducer, eventDetailsReducer } from './reducers/eventReducers'
+import { userLoginReducer } from './reducers/userReducers'
 
 
 const rootReducer = combineReducers({
   eventList: eventListReducer,
   eventDetails: eventDetailsReducer,
-  cart: cartReducer
+  cart: cartReducer,
+  userLogin: userLoginReducer,
 
 })
 const cartItemsFromStorage = localStorage.getItem('cartItems') ? 
 JSON.parse(localStorage.getItem('cartItems')) : []
 
+const userInfoFromStorage = localStorage.getItem('userInfo') ?
+JSON.parse(localStorage.getItem('userInfo')) : null
+
 const initialState = {
-  cart: {cartItems: cartItemsFromStorage}
+  cart: {cartItems: cartItemsFromStorage},
+  userLogin: {userInfo: userInfoFromStorage}
 }
+
+
 
 const store = configureStore({
   reducer: rootReducer,
