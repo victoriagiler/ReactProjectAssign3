da593cce995a36242648f670868e93c394eec043
Katie Park <katie.sang.park@gmail.com>
2023-02-12 11:12:43 -0800

    add Heroku

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import path from 'path'
 import connectDB from './config/db.js'
 import eventRoutes from './routes/eventRoutes.js'
 import errorHandler from './middleware/errormiddleware.js'
@@ -18,7 +19,17 @@ app.get('.api/config/paypal', (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 )
 
+const __dirname = path.resolve()
+if (process.env.NODE_ENV === 'production'){
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  })
+}
+
 app.use(errorHandler)
+
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, console.log(`Server is running on port ${PORT}`))
